2b385047231972840a87f4aac4f59d59388f7740
TristanKatte <tristankatte@hotmail.com>
2025-01-28 02:50:38 +0100

    added css

--- myapp/src/routes/+layout.svelte
+++ myapp/src/routes/+layout.svelte
@@ -5,6 +5,8 @@
     import 'open-props/style'
     import 'open-props/normalize'
     import 'open-props/buttons'
+
+    import '../app.css'
 </script>
 
 <div class="layout">
@@ -18,3 +20,23 @@
 
 </div>
 
+<style>
+    .layout {
+        height: 100%;
+        max-width: 1440px;
+        display: grid;
+        grid-template-columns: auto 1fr auto;
+        margin-inline: auto;
+        padding-inline: var(--size-7);
+    }
+
+    main {
+        padding-block: var(--size-9);
+    }
+
+    @media (min-width: 1440px) {
+        .layout {
+            padding-inline: 0;
+        }
+    }
+</style>
