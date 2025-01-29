a3a429f8d152e9782bccb907c193a5a6b4794338
TristanKatte <tristankatte@hotmail.com>
2025-01-29 17:57:47 +0100

    added view transition

--- myapp/src/routes/toggle.svelte
+++ myapp/src/routes/toggle.svelte
@@ -1,29 +1,26 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition'
-	import { Moon, Sun } from 'lucide-svelte'
 	import { theme, toggleTheme } from '$lib/theme'
 </script>
 
 <button on:click={toggleTheme} aria-label="Toggle theme">
-	{#if theme.current === 'dark'}
-		<div in:fly={{ y: 10 }}>
-			<Sun />
+	{#if theme.current === 'light'}
+		<div>
 			<span>Light</span>
 		</div>
 	{:else}
-		<div in:fly={{ y: -10 }}>
-			<Moon />
-			<span>Dark</span>
+		<div>
+			<span>Toggle</span>
 		</div>
 	{/if}
 </button>
 
 <style>
 	button {
-		padding: 0;
+		padding: var(--size-4);
 		font-weight: inherit;
 		background: none;
-		border: none;
+		border: 2px solid var(--orange-7);
+		border-radius: 50px;
 		box-shadow: none;
 		overflow: hidden;
 
@@ -31,5 +28,10 @@
 			display: flex;
 			gap: var(--size-2);
 		}
+	}
+
+	button:hover {
+		background-image: var(--gradient-6);
+		
 	}
 </style>
