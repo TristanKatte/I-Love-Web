9fe2f317f577c16df3365d59f0635173158b9bbc
TristanKatte <tristankatte@hotmail.com>
2025-01-29 03:24:57 +0100

    added syntax highlights

--- myapp/svelte.config.js
+++ myapp/svelte.config.js
@@ -1,10 +1,25 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import { mdsvex } from 'mdsvex'
+import { mdsvex, escapeSvelte  } from 'mdsvex'
+import { createHighlighter } from 'shiki'
 
-/** @type {import('mdsvex').mdsvexOptions} */
+/** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.md'],
+	layout: {
+		_: './src/mdsvex.svelte'
+	},
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await createHighlighter({
+				themes: ['poimandres'],
+				langs: ['javascript', 'typescript']
+			})
+			await highlighter.loadLanguage('javascript', 'typescript')
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
+			return `{@html \`${html}\` }`
+		}
+	},
 }
 
 /** @type {import('@sveltejs/kit').Config} */
